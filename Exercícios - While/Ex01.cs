143856d5190b8c1045e7db35c4ce993eccd4464a
Vinícius Araújo <viniciusaraujom@outlook.com>
2022-02-28 15:59:30 -0300

    Update Ex01.cs

--- Exercícios - While/Ex01.cs
+++ Exercícios - While/Ex01.cs
@@ -1,24 +1,30 @@
 /* Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha
 incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser
-impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002. */ 
+impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002. */
+using System;
 
-using System; 
+namespace Exercicios_While
+{
 
-namespace Exercicios_While {
+	class ex01
+	{
 
-	class	ex01 {
-	
-	static void Main(string[] args)
-		
-		int senha = 2002;
-		int usuario = int.Parse(Console.ReadLine());
-		
-		While (usuario != senha) {
-			Console.WriteLine("Senha inválida");
-		
+		static void Main(string[] args)
+		{
+
+
+			int senha = 2002;
+			int usuario = int.Parse(Console.ReadLine());
+
+			while (usuario != senha)
+			{
+				Console.WriteLine("Senha inválida!");
+				usuario = int.Parse(Console.ReadLine());
+			}
+
+			Console.WriteLine("Acesso permitido!");
+
 		}
-		
-		Console.WriteLine("Acesso permitido!");
 	}
-	
+
 }
