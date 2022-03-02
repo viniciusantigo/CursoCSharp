987caf2d4a6965454456c9732e03fad3bfe4ea12
Vinícius Araújo <viniciusaraujom@outlook.com>
2022-03-02 11:48:27 -0300

    Add files via upload

---
+++ Exercícios - For/Ex05.cs
@@ -0,0 +1,24 @@
+﻿/* Ler um valor N. Calcular e escrever seu respectivo fatorial. Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.
+Lembrando que, por definição, fatorial de 0 é 1. */
+
+using System;
+
+namespace Exercicios_For
+{
+    class Ex05
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int fatorial = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+                fatorial *= i;
+            }
+
+            Console.WriteLine(fatorial);
+        }
+    }
+}
