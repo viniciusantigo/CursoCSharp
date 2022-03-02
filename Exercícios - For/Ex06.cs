10634c90684036416d1e0c367ab5d5950de3ddd2
Vinícius Araújo <viniciusaraujom@outlook.com>
2022-03-02 12:07:20 -0300

    Add files via upload

---
+++ Exercícios - For/Ex06.cs
@@ -0,0 +1,22 @@
+﻿/* Ler um número inteiro N e calcular todos os seus divisores. */
+
+using System;
+
+namespace Exercicio_For
+{
+    class Ex06
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; n <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
