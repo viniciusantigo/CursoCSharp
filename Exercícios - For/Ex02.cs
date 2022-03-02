d0c23a19a1ba28d100e4a5a46304c703437678bf
Vinícius Araújo <viniciusaraujom@outlook.com>
2022-03-01 21:13:22 -0300

    Add files via upload

---
+++ Exercícios - For/Ex02.cs
@@ -0,0 +1,36 @@
+﻿/* Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida.
+Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo, mostrando
+essas informações conforme exemplo (use a palavra "in" para dentro do intervalo, e "out" para fora do intervalo).*/
+using System;
+
+namespace Exercicio_For
+{
+    class Ex02
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int x = 0;
+            int fora = 0;
+            int dentro = 0;
+
+            for(int i = 1; i <= n; i++)
+            {
+                x = int.Parse(Console.ReadLine());
+
+                if (x >= 10 && x <= 20)
+                {
+                    dentro++;
+                }
+
+                else
+                {
+                    fora++;
+                }
+            }
+
+            Console.WriteLine($"{dentro} in");
+            Console.WriteLine($"{fora} out");
+        }
+    }
+}
---
+++ Exercícios - For/Ex03.cs
@@ -0,0 +1,31 @@
+﻿/* Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de teste consiste
+de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para cada um destes
+conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem
+peso 5. */
+
+using System;
+using System.Globalization;
+
+namespace Exercicio_For
+{
+    class Ex03
+    {
+        static void Main(string[] args)
+        {
+            int usuario = int.Parse(Console.ReadLine());
+            double media = 0;
+
+            for (int i = 0; i < usuario; i++)
+            {
+                string[] vetor = (Console.ReadLine().Split(' '));
+                double n1 = double.Parse(vetor[0], CultureInfo.InvariantCulture);
+                double n2 = double.Parse(vetor[1], CultureInfo.InvariantCulture);
+                double n3 = double.Parse(vetor[2], CultureInfo.InvariantCulture);
+
+                media = (n1 * 2 + n2 * 3 + n3 * 5) / 10;
+
+                Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
